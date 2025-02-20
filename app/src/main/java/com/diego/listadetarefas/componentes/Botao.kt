2d07dd2eb23177affb044fc336b74804f62a43a0
Diego Cardoso <diegocar448@hotmail.com>
2025-02-20 15:54:43 -0300

    layout SalvarTarefa

---
+++ app/src/main/java/com/diego/listadetarefas/componentes/Botao.kt
@@ -0,0 +1,30 @@
+package com.diego.listadetarefas.componentes
+
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import com.diego.listadetarefas.ui.theme.LIGHT_BLUE
+import com.diego.listadetarefas.ui.theme.WHITE
+
+@Composable
+fun Botao(
+    onClick: () -> Unit,
+    modifier: Modifier,
+    texto: String
+){
+
+    Button(
+        onClick = {},
+        modifier = modifier,
+        colors = ButtonDefaults.buttonColors(
+            containerColor = LIGHT_BLUE,
+            contentColor = WHITE
+        )
+    ){
+        Text(text = texto, fontWeight = FontWeight.Bold, fontSize = 18.sp)
+    }
+}
--- app/src/main/java/com/diego/listadetarefas/view/SalvarTarefa.kt
+++ app/src/main/java/com/diego/listadetarefas/view/SalvarTarefa.kt
@@ -4,13 +4,17 @@ package com.diego.listadetarefas.view
 import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.RadioButtonDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -20,14 +24,22 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.diego.listadetarefas.componentes.Botao
 import com.diego.listadetarefas.componentes.CaixaDeTexto
 import com.diego.listadetarefas.ui.theme.Purple700
+import com.diego.listadetarefas.ui.theme.RADIO_BUTTON_GREEN_DISABLED
+import com.diego.listadetarefas.ui.theme.RADIO_BUTTON_GREEN_SELECTED
+import com.diego.listadetarefas.ui.theme.RADIO_BUTTON_RED_DISABLED
+import com.diego.listadetarefas.ui.theme.RADIO_BUTTON_RED_SELECTED
+import com.diego.listadetarefas.ui.theme.RADIO_BUTTON_YELLOW_DISABLED
+import com.diego.listadetarefas.ui.theme.RADIO_BUTTON_YELLOW_SELECTED
 import com.diego.listadetarefas.ui.theme.WHITE
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
@@ -36,12 +48,6 @@ import com.diego.listadetarefas.ui.theme.WHITE
 fun SalvarTarefa(
     navController: NavController
 ){
-
-    var tituloTarefa by remember {
-        mutableStateOf("")
-    }
-
-
 
     Scaffold(
         topBar = {
@@ -58,6 +64,29 @@ fun SalvarTarefa(
             )
         }
     ){ paddingValues -> // Recebe o padding fornecido pelo Scaffold
+
+        var tituloTarefa by remember {
+            mutableStateOf("")
+        }
+        var descricaoTarefa by remember {
+            mutableStateOf("")
+        }
+        var semPrioridadeTarefa by remember {
+            mutableStateOf(false)
+        }
+
+        var prioridadeBaixaTarefa by remember {
+            mutableStateOf(false)
+        }
+
+        var prioridadeMediaTarefa by remember {
+            mutableStateOf(false)
+        }
+
+        var prioridadeAltaTarefa by remember {
+            mutableStateOf(false)
+        }
+
         Column(
             Modifier
                 .fillMaxSize()
@@ -82,6 +111,71 @@ fun SalvarTarefa(
                     keyboardType = KeyboardType.Text
                 )
             )
+
+            CaixaDeTexto(
+                value = descricaoTarefa,
+                onValueChange = { descricaoTarefa = it },
+                modifier = Modifier
+                    .fillMaxWidth().height(150.dp) // Garante 100% da largura da tela
+                    .padding(20.dp, 10.dp, 20.dp, 0.dp), // Adiciona um espaço nas laterais
+                label = {
+                    Text("Descrição")
+                },
+                maxLines = 5,
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Text
+                )
+            )
+
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Center,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text(text = "Nível de prioridade")
+
+                RadioButton(
+                    selected = prioridadeBaixaTarefa,
+                    onClick = {
+                        prioridadeBaixaTarefa = !prioridadeBaixaTarefa
+                    },
+                    colors = RadioButtonDefaults.colors(
+                        unselectedColor = RADIO_BUTTON_GREEN_DISABLED,
+                        selectedColor = RADIO_BUTTON_GREEN_SELECTED
+                    )
+                )
+
+                RadioButton(
+                    selected = prioridadeMediaTarefa,
+                    onClick = {
+                        prioridadeMediaTarefa = !prioridadeMediaTarefa
+                    },
+                    colors = RadioButtonDefaults.colors(
+                        unselectedColor = RADIO_BUTTON_YELLOW_DISABLED,
+                        selectedColor = RADIO_BUTTON_YELLOW_SELECTED
+                    )
+                )
+
+                RadioButton(
+                    selected = prioridadeAltaTarefa,
+                    onClick = {
+                        prioridadeAltaTarefa = !prioridadeAltaTarefa
+                    },
+                    colors = RadioButtonDefaults.colors(
+                        unselectedColor = RADIO_BUTTON_RED_DISABLED,
+                        selectedColor = RADIO_BUTTON_RED_SELECTED
+                    )
+                )
+            }
+
+            Botao(
+                onClick = {
+
+                },
+                modifier = Modifier.fillMaxWidth().height(80.dp).padding(20.dp),
+                texto = "Salvar"
+            )
+
         }
     }
 
